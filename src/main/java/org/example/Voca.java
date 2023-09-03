14f478a582f9640ef5865dddeb981c22ce98e569
SionLee21 <sionl1030@gmail.com>
2023-09-03 12:47:14 +0900

    Create Voca.java

---
+++ src/main/java/org/example/Voca.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Voca {
+    public static void main(String[] args) {
+        Voca myVoca = new Voca();
+        myVoca.run();
+    }
+    public void run(){
+
+    }
+}
+
+
